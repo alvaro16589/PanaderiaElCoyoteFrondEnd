adc01f4bd98dcb82e8c7e2b05d142399ec93cfb6
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2021-09-29 22:41:48 -0400

    modificando el css del home en su carrusel para hacerlo adaptarivo a los diferentes tipos de pantallas

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title = "Panaderia El Coyote";
+  title = "El Coyote";
   constructor() { }
 
   ngOnInit(): void {
